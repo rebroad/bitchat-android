ee9736cc4b99f120f2909983df547f09a4c7de96
iAmEben <justiceebenezer4@gmail.com>
2025-07-13 15:00:16 +0100

    Original commits

--- app/src/main/java/com/bitchat/android/MainActivity.kt
+++ app/src/main/java/com/bitchat/android/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.repeatOnLifecycle
